415c2c86fefa3ebf607ccd23d28cada22e8ed997
DibakarMSFT <dibakardharchoudhury@yahoo.co.in>
2026-08-07 11:10:31 +0200

    Version badge: move to bottom-right (no longer covers footer) + show build date & time; v10

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Minimal offline shell for the OPB Check-in PWA. Scanning + registration require
 // the network (camera + backend), so we only cache the static shell here.
-const VERSION = "v9";
+const VERSION = "v10";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./mandala.svg", "./version.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
