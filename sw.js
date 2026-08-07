39093f058d346505d9d9a13b9bfbeb41867491e5
DibakarMSFT <dibakardharchoudhury@yahoo.co.in>
2026-08-07 12:22:33 +0200

    v1.8: schema-driven Guest List table (dynamic columns per worksheet)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Minimal offline shell for the OPB Check-in PWA. Scanning + registration require
 // the network (camera + backend), so we only cache the static shell here.
-const VERSION = "v1.7";
+const VERSION = "v1.8";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./mandala.svg", "./howrah.svg", "./version.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
